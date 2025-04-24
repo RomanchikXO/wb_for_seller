6f016291c5620017893794796ddc6fe18b48171e
Роман Дикарев <super.romanchik228@yandex.ru>
2025-04-24 23:01:16 +0300

    fix

---
+++ django_app/wait_for_db.py
@@ -0,0 +1,32 @@
+import time
+import psycopg2
+from psycopg2 import OperationalError
+from loader import POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD
+
+def wait_for_db():
+    db_name = POSTGRES_DB
+    db_user = POSTGRES_USER
+    db_password = POSTGRES_PASSWORD
+    db_host = 'db',
+    db_port = 5432,
+
+    print("⏳ Waiting for PostgreSQL to be ready...")
+
+    while True:
+        try:
+            conn = psycopg2.connect(
+                dbname=db_name,
+                user=db_user,
+                password=db_password,
+                host=db_host,
+                port=db_port,
+            )
+            conn.close()
+            print("✅ PostgreSQL is available!")
+            break
+        except OperationalError:
+            print("🔁 PostgreSQL not ready yet, retrying in 1s...")
+            time.sleep(1)
+
+if __name__ == "__main__":
+    wait_for_db()
