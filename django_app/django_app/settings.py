aaedaacd2ec604042e22682ca60828ea7f34385f
Роман Дикарев <super.romanchik228@yandex.ru>
2025-04-25 00:04:09 +0300

    fix compose

--- django_app/django_app/settings.py
+++ django_app/django_app/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myapp.apps.MyappConfig'
+    'django_app.myapp.apps.MyappConfig'
 ]
 
 MIDDLEWARE = [
