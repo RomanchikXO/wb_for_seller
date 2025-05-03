77373750c5cf468dccdb80e13af5901d7b287643
Роман Дикарев <super.romanchik228@yandex.ru>
2025-05-03 20:59:31 +0300

    change repricer.html and main/views

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from decorators import login_required_cust
-
+from myapp.models import Price, WbLk
+from django.core.paginator import Paginator
 
 @login_required_cust
 def main_view(request):
@@ -9,4 +10,17 @@ def main_view(request):
 
 @login_required_cust
 def repricer_view(request):
-    return render(request, 'repricer.html')
+    user = request.user_obj  # получаем объект юзера из нашего кастомного декоратора
+    per_page = int(request.GET.get('per_page', 10))
+    page_number = request.GET.get('page', 1)
+
+    prices = Price.objects.filter(lk__groups=user.groups).select_related('lk')
+
+    paginator = Paginator(prices, per_page)
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'repricer.html', {
+        'page_obj': page_obj,
+        'per_page': per_page,
+        'paginator': paginator,
+    })
