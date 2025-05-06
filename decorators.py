7fc9d7d644db1ccee4be1a95d7c70f5afbd30f27
Роман Дикарев <super.romanchik228@yandex.ru>
2025-05-06 13:51:55 +0300

    допилил декоратор сессии

--- decorators.py
+++ decorators.py
@@ -2,6 +2,8 @@ from log_context import task_context
 from functools import wraps
 from django.shortcuts import redirect
 
+from myapp.models import CustomUser
+
 
 def with_task_context(task_name):
     def decorator(func):
@@ -17,10 +19,23 @@ def with_task_context(task_name):
 
 
 def login_required_cust(view_func):
+    """
+    todo Возможно надо будет удалить блок try except дабы ошибки не выбрасывали на страницу регистрации
+
+    Декоратор сессии
+    Args:
+        view_func:
+
+    Returns:
+
+    """
     @wraps(view_func)
     def wrapper(request, *args, **kwargs):
-        if request.session.get('user_id'):
-            return view_func(request, *args, **kwargs)
-        else:
+        try:
+            if request.session.get('user_id') and CustomUser.objects.get(id=request.session.get('user_id')):
+                return view_func(request, *args, **kwargs)
+            else:
+                return redirect(f"/login/?next={request.path}")
+        except:
             return redirect(f"/login/?next={request.path}")
     return wrapper
