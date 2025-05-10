e2a92710043f5972d8c8ccaeae16fa104ebffd25
Роман Дикарев <super.romanchik228@yandex.ru>
2025-05-10 20:25:52 +0300

    fix bot

--- cookie_updater/main.py
+++ cookie_updater/main.py
@@ -2,9 +2,9 @@ import asyncio
 from playwright_utils import login_and_get_context, get_and_store_cookies
 
 async def main():
-    # page = await login_and_get_context()  # ручной вход 1 раз
+    page = await login_and_get_context()  # ручной вход 1 раз
     while True:
-        # await get_and_store_cookies(page)
+        await get_and_store_cookies(page)
         await asyncio.sleep(300)  # каждые 5 минут
 
 if __name__ == "__main__":
