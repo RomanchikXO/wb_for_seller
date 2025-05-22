b374f233e04b909ae2184df6d50c4d3c8c5d1c90
Роман Дикарев <super.romanchik228@yandex.ru>
2025-05-22 16:49:57 +0300

    глобальные изменения функции репрайсера

--- tasks/set_price_on_wb_from_repricer.py
+++ tasks/set_price_on_wb_from_repricer.py
@@ -101,7 +101,7 @@ def get_price(
     try:
         price_without_disc = math.ceil(solutions[0])
     except Exception as e:
-        logger.error(f"Ошибка в get_price: {e}. solution: {solutions}. Параметры: {locals()}")
+        logger.error(f"Ошибка в get_price: {e}. solution: {solutions}")
     response = math.floor(math.floor(price_without_disc * discount) * (1 - spp))
     return price_without_disc, response
 
@@ -113,6 +113,7 @@ def set_current_list(data: List[dict])-> dict:
         for i in data:
             if not response.get(i["token"]):
                 response[i["token"]] = []
+            logger.info(f'передаем {i["keep_price"]}, {i["cost_price"]}, {i["spp"]}, {i["discount"]}, {i["wallet_discount"]}, {i["nds"]}, {i["reject"]}, {i["commission"]}, {i["acquiring"]}, {i["drr"]}, {i["usn"]}')
             price, black_price = get_price(i["keep_price"], i["cost_price"], i["spp"], i["discount"], i["wallet_discount"], i["nds"], i["reject"], i["commission"], i["acquiring"], i["drr"], i["usn"])
             red_price = math.floor(black_price * (1 - int(i["wallet_discount"])/100))
             response[i["token"]].append(
