8d7a4ee28d16ba3e88eec4a0a13942e22d346d60
Роман Дикарев <super.romanchik228@yandex.ru>
2025-08-27 21:28:16 +0300

    теперь бот выводит id юзера

--- BOT/handlers/default_handlers/start.py
+++ BOT/handlers/default_handlers/start.py
@@ -5,7 +5,7 @@ from states import get_status, set_status
 
 @bot.message_handler(commands=["start"])
 def start_handler(message: Message):
-    bot.send_message(message.chat.id, f"Привет, я Бот!")
+    bot.send_message(message.chat.id, f"Привет, я Бот! Твой ТГ ID: {message.from_user.id}")
 
 
 @bot.message_handler(func=lambda message: get_status(message.from_user.id) == 'get_sms_code')
