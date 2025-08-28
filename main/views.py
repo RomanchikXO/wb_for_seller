b6e3fbbf500c897caf028ea0581a0b66ce02c6c0
Роман Дикарев <super.romanchik228@yandex.ru>
2025-08-28 21:39:59 +0300

    чуть накидал какашек в код чтобы подравнять рек поставку

--- main/views.py
+++ main/views.py
@@ -1175,6 +1175,10 @@ def podsort_view(request):
                 sum_rec_all = sum(list(map(lambda x: x["rec_delivery"], subitems))) #сумма поставок без фильтров
                 diff = sum_rec_all - total_short_rec_del[i["article"]]
                 coef = diff / sum_rec_all - 1
+                logger.info(coef)
+                logger.info(sum_rec_all)
+                logger.info(total_short_rec_del[i["article"]])
+                logger.info(copy_data[index]["subitems"])
                 coef *= -1 if (sum_rec_all < 0 and total_short_rec_del[i["article"]]>0) else 1
 
                 change_index = 0 #индекс по которому мы изменим поставку у товара в случае разницы
